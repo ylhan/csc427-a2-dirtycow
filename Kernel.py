dd500ce9670acb837ac02f22d037b69a6e94fa0f
Allen <allenylhan@gmail.com>
2021-03-06 04:42:26 -0500

    Work in progress

--- Kernel.py
+++ Kernel.py
@@ -4,291 +4,149 @@ import time
 import threading
 from enum import Enum
 
-class Permissions(Enum):
+class Flags(Enum):
     READ_ONLY = 1
-    FOLL_WRITE = 2
+    WRITE = 2
+    COW = 3 # if someone tries to write, create a copy
+    PRIVATE = 4 # anything marked private is writable
 
-class vm_area_struct:
+class vm_page:
     '''
-    http://lxr.linux.no/linux+v4.8/include/linux/mm_types.h#L300 
+    Represents a Virtual Memory (vm) page and also some functionality of a 
+    page table
 
-    This struct defines a memory VMM memory area. There is one of these
-    per VM-area/task.  A VM area is any part of the process virtual memory
-    space that has a special rule for the page-fault handlers (ie a shared
-    library, the executable area etc).
+    Very roughly similar to 
+    http://lxr.linux.no/linux+v4.8/include/linux/mm_types.h#L300
+    '''
+    def __init__(self, page_prot, data, file, offset, policy, evicted=False, dirty=False):
+        self.page_prot = page_prot # page protection (READ ONLY)
+        self.data = data # What this page actually holds
+        self.file = file # The file that backs this page
+        self.offset = offset # Offset in that file
+        self.policy = policy # policy if someone tries to write to this page
+        self.evicted = evicted # if this page is in memory or not
+        self.dirty = dirty
+
+    def sync(self):
+        '''
+        If this page is backed by a file, syncs any changes to this page to
+        disk
+        '''
+        if self.dirty and self.file:
+            with open(self.file, 'r+') as f:
+                content = list(f.readlines()[0])
+                f.seek(0)
+                f.truncate
+                # Seek to our offset and replace the data there
+                i = self.offset
+                for c in self.data:
+                    if i < len(content):
+                        content[i] = c
+                    else:
+                        break
+                f.write("".join(content))
+class Memory:
+    '''
+    Represents the Memory of the program (collection of pages)
     '''
     def __init__(self):
-        vm_file = vm_file # File backing this mapping
-        vm_prot = PERMISSIONS.READ_ONLY# Abstraction/similar to vm_page_prot
+        self.memory = []
 
+    def mmap(self, file, page_prot, policy):
+        '''
+        Maps a file into memory
+        '''
+        with open(file, 'r') as f:
+            content = f.readlines()[0]
+            for i in range(0, len(content), 4):
+                data = ""
+                for j in range(i, min(i+4, len(content))):
+                    data += content[j]
+                page = vm_page(page_prot, data, file, i, policy)
+                self.memory.append(page)
+
+    def peek(self):
+        for page in self.memory:
+            print(page.offset, page.data)
+
+    def sync(self):
+        '''
+        Syncs any changes to pages that are backed by a file back to the hard
+        disk
+        '''
+        for page in self.memory:
+            page.sync()
 class Kernel:
-    
+    '''
+    Represents functions and operations in the Kernel
+
+    This is really simplified and I combined the functionality of a lot of
+    different kernel operations into a few functions for simplicity.
+    ''' 
+
     def __init__(self):
-        with open(f, 'r') as file:
-            self._memory = list(file.read())
-            self._root_mem_bound = len(self._memory)
-            self._memory.extend(['u']*(self._root_mem_bound))
-        self._file = f
-        self._write_pointer = 0
-        self.lock = threading.Lock()
+        self.memory = Memory()
 
-    def os_scheduler(self):
-        ''' TODO: Pseudo Randomly schedules execution and simulate the CPU
+    def mem_write(self, buf):
         '''
-        pass
-
-    def lru_cache_eviction(self):
-        ''' TODO: write the eviction policy for least recently used page
-        evicted pages MUST
-        '''
-        pass
-
-    def semaphore_lock(self, conditional):
-        ''' TODO: Implement a semaphore that locks and unlocks based on a conditional
-        '''
-        pass
-
-    def semaphore_unlock(self, conditional):
-        ''' TODO: Implement a semaphore that locks and unlocks based on a conditional
-        '''
-        pass
-
-    def _write(self, buffer):
-        j = 0
-        for i in range(self._write_pointer, self._write_pointer+len(buffer)):
-            self._memory[i] = buffer[j]
-            j += 1
-
-    def _copy_on_write(self, addr, buffer):
-        self._write_pointer = addr+self._root_mem_bound
-        self._memory[self._root_mem_bound:]= self._memory[:self._root_mem_bound]
-        self._write(buffer)
-
-    def dontneed(self, addr, length):
-        self.lock.acquire()
-        self._write_pointer = int(addr, 16)
-        # Write back to disk
-        if self._write_pointer < 0:
-            raise AddressError("You can't write to a negative address.")
-
-        if self._write_pointer >= len(self._memory):
-            raise AddressError(f"The address you are trying to write to is >= {len(self._memory)} (size of memory).")
-
-        if self._write_pointer >= self._root_mem_bound:
-            print("No write permissions to rootfile discarding memory...")
-            self._memory[self._root_mem_bound:] = ['u']*(self._root_mem_bound)
-            return
-
-        if self._write_pointer < self._root_mem_bound and self._write_pointer + length > self._root_mem_bound:
-            raise AddressError("Length too long. No write permissions to rootfile discarding memory...")
+        Special function that is called when writing to /proc/self/mem
+        This function attempt to write to the memory location of the read only
+        file and tries to handle any errors associate with that.
         
-        with open(self._file, 'w') as file:
-            out = "".join(self._memory[self._write_pointer:self._write_pointer+length])
-            file.write(out)
-        self.lock.release()
-
-    def peek(self, col=8):
-        if col <= 0:
-            print(f"Provided columns per row must be > 0")
-            return
-
-        print(f"Read only bounds: {0:08x}...{self._root_mem_bound-1:08x}")
-        print(f"Free bounds:      {self._root_mem_bound-1:08x}...{len(self._memory)-1:08x}")
-        print("=== Memory ===")
-        for i in range(0, len(self._memory), col):
-            out = f"0x{i:08x}: "
-            for j in range(i, min(i+col, len(self._memory))):
-                out += f" {self._memory[j]}"
-            print(out)
-
-    def load(self, file):
+        http://lxr.linux.no/linux+v4.8/fs/proc/base.c#L903
+        http://lxr.linux.no/linux+v4.8/fs/proc/base.c#L845
         '''
-        This is an abstraction
-
-        The proc filesystem is a pseudo-filesystem which provides an
-        interface to kernel data structures.  It is commonly mounted at
-        /proc.  Typically, it is mounted automatically by the system, but
-        it can also be mounted manually using a command such as:
-
-            mount -t proc proc /proc
-
-        Most of the files in the proc filesystem are read-only, but some
-        files are writable, allowing kernel variables to be changed.
-        '''
-
-    def handle_mem_swap(self, buffer):
-        addr = int(addr, 16)
-        if addr < 0:
-            raise AddressError("You can't write to a negative address.")
-
-        if addr >= len(self._memory):
-            raise AddressError(f"The address you are trying to write to is >= {len(self._memory)} (size of memory).")
-
-        if addr >= self._root_mem_bound and len(buffer) > (len(self._memory)-addr):
-            raise MemoryError(f"Your buffer is too large to fit in memory at your given address.")
-
-        if addr < self._root_mem_bound and len(buffer) > (self._root_mem_bound-addr):
-            raise MemoryError(f"Your buffer is too large to fit in memory at your given address.")
-
-        # Write to user memory
-        if addr >= self._root_mem_bound:
-            self._write(addr, buffer)
         
-        # Writing to root memory: copy on write
-        if addr < self._root_mem_bound:
-            self._copy_on_write(addr, buffer)
+        # try to access memory 
+            # try to write one page at a time
 
-    def __access_remote_vm(self, task_struct):
-        ''' Access another process' address space as given in mm.  If non-NULL, use the
-            given task for page fault accounting.
-            https://code.woboq.org/linux/linux/mm/memory.c.html#__access_remote_vm
+    def access_remote_vm(self, buf, write):
         '''
-        vm_area_struct = None
-        old_buf = None 
-        write = gup_flags & Permissions.FOLL_WRITE
-        down_read(mmmmap_sem)
-        # ignore errors, just check how much was successfully transferred */
-        while (_len):
-            maddr
-            page = None
-            ret = get_user_pages_remote(tsk, mm, addr, 1,gup_flags, page, vma, NULL)
-            if (ret <= 0):
-            #ifndef CONFIG_HAVE_IOREMAP_PROT
-                break
-            #else
-                vma = find_vma(mm, addr)
-                if (not vma or vma.vm_start > addr):
-                    break
-                if (vma.vm_ops and vma.vm_ops.access):
-                    ret = vma.vm_ops.access(vma, addr, buf,
-                                len, write)
-                if (ret <= 0):
-                    break
-                bytes = ret
-            #endif
-            else:
-                bytes = len
-                offset = addr & (PAGE_SIZE-1)
-                if (bytes > PAGE_SIZE-offset):
-                    bytes = PAGE_SIZE-offset
-                maddr = kmap(page)
-                if (write):
-                    copy_to_user_page(vma, page, addr,
-                            maddr + offset, buf, bytes)
-                    set_page_dirty_lock(page)
-                else :
-                    copy_from_user_page(vma, page, addr, buf, maddr + offset, bytes)
-                kunmap(page)
-                put_page(page)
-            len -= bytes
-            buf += bytes
-            addr += bytes
-        up_read(mm.mmap_sem)
-        return buf - old_buf
+        Actual function that access or write to the virtual memory (vm)
+        of our program.
 
-    def __get_user_pages(tsk, mm,start, nr_pages, gup_flags, pages, vmas, nonblocking):
+        Recall from CSC369 that memory is divided into pages (4096 bytes). We
+        are limited to getting 4096 bytes at a time.
+
+        In this exercise we're limited to 4 characters at a time.
+
+        http://lxr.linux.no/linux+v4.8/mm/memory.c#L3854
         '''
-        __get_user_pages() - pin user pages in memory
-        @tsk:        task_struct of target task
-        @mm:         mm_struct of target mm
-        @start:      starting user address
-        @nr_pages:   number of pages from start to pin
-        @gup_flags:  flags modifying pin behaviour
-        @pages:      array that receives pointers to the pages pinned.
-                    Should be at least nr_pages long. Or NULL, if caller
-                    only intends to ensure the pages are faulted in.
-        @vmas:       array of pointers to vmas corresponding to each page.
-                    Or NULL if the caller does not require them.
-        @nonblocking: whether waiting for disk IO or mmap_sem contention
-        Returns number of pages pinned. This may be fewer than the number
-        requested. If nr_pages is 0 or negative, returns 0. If no pages
-        were pinned, returns -errno. Each page returned must be released
-        with a put_page() call when it is finished with. vmas will only
-        remain valid while mmap_sem is held.
-        Must be called with mmap_sem held.
-        http://lxr.linux.no/linux+v4.8/mm/gup.c#L519 
+        # while not finish writing
+        # get a page and write to it
+
+    def __get_user_pages(self, gup_flags):
         '''
-        i = 0
-        page_mask = Nopne
-        vma = None
+        Tries to find the page that we're looking for in memory, if
+        not try to fault in the page.
 
-        if (not nr_pages):
-            return 0
+        HINT: If the permission of the page and our request flag don't match
+        we will also try to fault in the page by copying it (copy on write) so
+        we can return a valid page
 
-        VM_BUG_ON(-pages != - (gup_flags & FOLL_GET))
+        :gup_flags (Get User Pages flags) encodes information about why the 
+                    caller wants the page and what they intend to do with it
 
-        while True:
-            if (-vma or start >= vma.vm_end) :
-                vma = find_extend_vma(mm, start)
-                if (-vma and in_gate_area(mm, start)):
-                    ret = get_gate_page(mm, start & PAGE_MASK,gup_flags, vma, pages)
-                    if (ret):
-                        return i
-                    page_mask = 0
-
-                if (-vma or check_vma_flags(vma, gup_flags)):
-                        return -EFAULT
-                if (is_vm_hugetlb_page(vma)):
-                        i = follow_hugetlb_page(mm, vma, pages, vmas,)
-                        continue
-
-
-            if (unlikely(fatal_signal_pending(current))):
-                    return i
-            cond_resched()
-            page = follow_page_mask(vma, start, foll_flags, page_mask)
-    
-            if (vmas):
-                vmas[i] = vma
-                page_mask = 0
-            page_increm = 1 + (~(start >> PAGE_SHIFT) & page_mask)
-            i += page_increm
-            start += page_increm * PAGE_SIZE
-            nr_pages -= page_increm
-        return i
-
-
-    def __faultin_page(tsk, vma, address, flags, nonblocking):
+        http://lxr.linux.no/linux+v4.8/mm/gup.c#L519
         '''
-        mmap_sem must be held on entry.  If @nonblocking != NULL and
-       @flags does not include FOLL_NOWAIT, the mmap_sem may be released.
-        If it is, *@nonblocking will be set to 0 and -EBUSY returned.
-        http://lxr.linux.no/linux+v4.8/mm/gup.c#L519 
+        # try to get the page
+        # if getting the page failed 
+           # try to fault in the page
+           # if it fails try again
+           # yay we got a valid page
+
+    def faultin_page(self):
         '''
-        i = 0
-        page_mask = None
-        vma = None
+        Tries to fault in the request page either to match requested permissions
+        or load it from disk
+        http://lxr.linux.no/linux+v4.8/mm/gup.c#L354
+        '''
+        # call handle mm fault to create new page or get it
+        # remove write flag to prevent infinite loop
 
-        if (not nr_pages):
-            return 0
-
-        VM_BUG_ON(-pages != - (gup_flags & FOLL_GET))
-
-        if (-vma or start >= vma.vm_end) :
-            vma = find_extend_vma(mm, start)
-            if (-vma and in_gate_area(mm, start)):
-                ret = get_gate_page(mm, start & PAGE_MASK,gup_flags, vma, pages)
-                if (ret):
-                    return i
-                page_mask = 0
-        if (vmas):
-            vmas[i] = vma
-            page_mask = 0
-        page_increm = 1 + (~(start >> PAGE_SHIFT) & page_mask)
-        i += page_increm
-        start += page_increm * PAGE_SIZE
-        nr_pages -= page_increm
-
-        if (-vma or check_vma_flags(vma, gup_flags)):
-                    return -EFAULT
-        if (is_vm_hugetlb_page(vma)):
-                i = follow_hugetlb_page(mm, vma, pages, vmas)
-
-        return i
-class MemoryError(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-
-class AddressError(Exception):
-    def __init__(self, message):
-        super().__init__(message)
+    def handle_mm_fault(self):
+        '''
+        http://lxr.linux.no/linux+v4.8/mm/memory.c#L3566
+        '''
+        # Get page and return it
+        pass
--- User.py
+++ User.py
@@ -1,21 +1,54 @@
 #!/usr/bin/python3
 
-from Kernel import kernel
+from Kernel import Kernel, Flags, Memory
 
-class User: 
+class UserSpace:
+    '''
+    Represents functions and operations available to a normal user.
+    ''' 
     
     def __init__(self):
         self.kernel = Kernel()
 
-    def write(self, file_descriptor, input, length):
+    def write(self, file_descriptor, input, length, file='/proc/self/mem'):
         '''
-        Write doesn't actually write to memory directly instead under the hood
-        it actually calls a 
+        Write is a non-atomic operation meaning that it takes *multiple* steps
+        to complete. 
+
+        For the sake of this exercise we'll assume that write always calls
+        mem_write. This is not (entirely) true and only applies IF you try to
+        write to /proc/self/mem.
+        
+        What is /proc/self/mem? In short, it's a pseudo file (doesn't really
+        exist on disk) that the kernel generates on the fly, this file represents
+        the memory of our current program.
+
+        Why /proc/self/mem? Recall that mem_write is only called when writing
+        to /proc/self/mem, if you try to write to something else another internal
+        write function is called. The bug that allows for the dirty cow exploit
+        to exist occurs in the call stack of mem_write, this is why we try to
+        write to /proc/self/mem/.
+
+        FUN FACT: ptrace was an alternative attack vector to /proc/self/mem
+        https://man7.org/linux/man-pages/man2/ptrace.2.html
+        https://github.com/nowsecure/dirtycow/blob/master/ptrace.c
+
+        For more context take a look at 
+        http://lxr.linux.no/linux+v4.8/fs/proc/base.c#L933
+        which defines operations for mem_write.
+
+        Also the manual page for proc:
+        https://man7.org/linux/man-pages/man5/procfs.5.html
+
         '''
         pass
 
-    def open(self, file):
-        self.kernel.load(file)
-
-    def mmap(self, file, memory_region, flag):
-        self.kernel.mmap(file, memory_region, flag)
+if __name__ == '__main__':
+    m = Memory()
+    m.mmap("read_only.txt", Flags.READ_ONLY, Flags.PRIVATE)
+    m.peek()
+    print()
+    m.memory[0].dirty = True
+    m.memory[0].data = "test"
+    m.sync()
+    m.peek()
